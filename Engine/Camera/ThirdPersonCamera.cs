5549165e181ab913b0618fc2d5776249437a8650
PatrykMarkowski <=>
2022-08-15 11:40:03 +0200

    loaders

---
+++ Engine/Camera/ThirdPersonCamera.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Mathematics;
+
+namespace Dwarf.Engine.Cameras;
+public class ThirdPersonCamera : Camera, ICamera {
+  public ThirdPersonCamera() { }
+
+  public ThirdPersonCamera(Vector3 position, float aspectRatio) : base(position, aspectRatio) { }
+
+  public void HandleMovement() {
+
+  }
+
+  internal override void UpdateVectors() {
+
+  }
+
+  public float GetAspectRatio() {
+    return AspectRatio;
+  }
+
+  public void SetAspectRatio(float aspectRatio) {
+    AspectRatio = aspectRatio;
+  }
+}
